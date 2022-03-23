ff83f6df6bd95ecbea5baa31357ae0865037a52b
Anat Mazar <anat.m@bizzabo.com>
2022-03-23 15:54:59 +0200

    create question and answer

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -27,14 +27,15 @@ module.exports = class KahootController {
       res.json(stats);
     });
 
-    app.get('/*', (req, res) => {
-      res.sendFile(this.path.join(__dirname, '../../build', 'index.html'));
-    });
-
     app.post('/trivialist/:triviaId/question', async (req, res) => {
       const question = await this.kahootService.createQuestion(req.params.triviaId, req.body.question, req.body.answers);
       res.json({ question });
     });
+
+    app.get('/*', (req, res) => {
+      res.sendFile(this.path.join(__dirname, '../../build', 'index.html'));
+    });
+
   }
 
   setNamespaceConnection(namespace, callback) {
