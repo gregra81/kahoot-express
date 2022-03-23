414c5897769b12987b7963fec3db39131dc7f415
Greg Rashkevitch <rashkevitch@gmail.com>
2022-03-23 15:07:33 +0200

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: `${__dirname}/.env` });
 const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
+const cors = require('cors');
 const { initKahootModule } = require('./src/module');
 const configureDI = require('./src/config/di');
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3030;
 const app = express();
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 const io = socketIO(server);
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.urlencoded({ extended: true }));
